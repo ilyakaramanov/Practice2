2307b19d0da97e8cbebd60811f7f423a94e7e2c2
ilyakaramanov <ilya.karamanov@gmail.com>
2018-11-29 00:06:36 +0200

    Initial commit

---
+++ src/ua/org/oa/ilyakaramanov/Main.java
@@ -0,0 +1,86 @@
+package ua.org.oa.ilyakaramanov;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public void task1() {
+        String s1 = "Первая";
+        String s2 = "Вторая";
+        String s3 = s1.substring(1).concat(s2.substring(1));
+        System.out.println(s3);
+    }
+
+    public void task2() {
+        String s1 = "Practice2";
+        String s2 = s1.substring(3, 6);
+        System.out.println(s2);
+    }
+
+    public void task3() {
+        String s1 = "Строка";
+        String s2 = s1.substring(s1.length() - 2).concat(s1.substring(0, s1.length() - 2));
+        System.out.println(s2);
+    }
+
+    public void task4() {
+        String s1 = "qwerty";
+        String s2 = "";
+        for (int i = 0; i < s1.length(); i++) {
+            s2 = s2 + s1.charAt(i) + s1.charAt(i);
+        }
+        System.out.println(s2);
+    }
+
+    public void task5() {
+        int count = 0;
+        String s1 = "bob is bab rfdbfb";
+        for (int i = 0; i < s1.length() - 1; i++) {
+            if ((s1.charAt(i) == 'b') && (s1.charAt(i + 2) == 'b')) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public void task6() {
+        String s1 = "Co*mput*er";
+        int index = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == '*') {
+                index = i;
+                s1 = s1.substring(0, index - 1) + s1.substring(index + 2);
+            }
+        }
+        System.out.println(s1);
+    }
+
+    public void task7() {
+        int count = 0;
+        String s1 = "dfgja sdfgns lkfga";
+        s1.toLowerCase();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == ' ') {
+                if (s1.charAt(i - 1) == 'a')
+                    count++;
+                if (s1.charAt(i - 1) == 's')
+                    count++;
+            }
+        }
+        if (s1.endsWith("s") || s1.endsWith("a")) {
+            count++;
+        }
+        System.out.println(count);
+    }
+
+    public void task8() {
+        String s1 = "qwertyuiop";
+        String s2 = "qwer";
+        String s3 = s1.replace(s2, "");
+        System.out.println(s3);
+
+    }
+}
+
+
---
+++ src/ua/org/oa/ilyakaramanov/Test.java
@@ -0,0 +1,24 @@
+package ua.org.oa.ilyakaramanov;
+
+public class Test {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.print("task1: ");
+        main.task1();
+        System.out.print("task2: ");
+        main.task2();
+        System.out.print("task3: ");
+        main.task3();
+        System.out.print("task4: ");
+        main.task4();
+        System.out.print("task5: ");
+        main.task5();
+        System.out.print("task6: ");
+        main.task6();
+        System.out.print("task7: ");
+        main.task7();
+        System.out.print("task8: ");
+        main.task8();
+
+    }
+}
